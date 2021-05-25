2f5bff7c6ae1a9ccc0bc9c11af6ffbc20785f23e
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-05-25 10:41:19 +0530

    Project Setup

---
+++ lib/defaults/constants.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+const kAppName = "deMeter";
+
+const kPrimaryColor = Color(0xFF206a5e);
+const kSecondaryColor = Color(0xFF80b241);
+
+const kTextColor = Color(0xFF08100f);
+const kBGColor = Color(0xFFF2FAF8);
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RootApp());
+}
+class RootApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
