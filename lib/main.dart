ea96b2f0c616c830a01c151d7504618c61154ccd
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-05-31 08:28:49 +0530

    Flip Card Animation in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,34 @@
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(RootApp());
 }
+
 class RootApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Container(),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: FlipCard(
+          direction: FlipDirection.HORIZONTAL, // change to VERTICAL for vertical animation
+          front: Container(
+            height: double.infinity,
+            width: double.infinity,
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            child: Text('Front'),
+          ),
+          back: Container(
+            height: double.infinity,
+            width: double.infinity,
+            alignment: Alignment.center,
+            color: Colors.redAccent,
+            child: Text('Back'),
+          ),
+        ),
+      ),
+    );
   }
 }
